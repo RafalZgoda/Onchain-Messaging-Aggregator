9a709a3b030c806e62da06f87d37797a0f29f46a
Nico <nicobdx@icloud.com>
2023-07-21 21:35:25 +0200

    add xmtp libs

---
+++ xmtp/libs/xmtp-libs.ts
@@ -0,0 +1,113 @@
+import {
+  Client as TXMTPClient,
+  DecodedMessage as TXMTPMessage,
+  Conversation as TXMTPConversation,
+} from "@xmtp/xmtp-js";
+import { JsonRpcSigner } from "@ethersproject/providers";
+
+export type TMessage = {
+  id: string;
+  senderAddress: string;
+  recipientAddress: string;
+  sent: Date;
+  content: string;
+  platform: string;
+};
+
+export const initXMTPClient = async function ({
+  signer,
+}: {
+  signer: JsonRpcSigner;
+}): Promise<TXMTPClient> {
+  const xmtp = await TXMTPClient.create(signer, { env: "production" });
+  return xmtp;
+};
+
+export const getConversationXMTP = async function ({
+  xmtp_client,
+  addressTo,
+}: {
+  xmtp_client: TXMTPClient;
+  addressTo: string;
+}): Promise<TXMTPConversation | null> {
+  //Creates a new conversation with the address
+  if (await xmtp_client?.canMessage(addressTo)) {
+    const conversation = await xmtp_client.conversations.newConversation(
+      addressTo
+    );
+    return conversation;
+  } else {
+    console.log("cant message because is not on the network.");
+    return null; // or throw error
+  }
+};
+
+export const getMessagesHistoryXMTP = async function ({
+  conversation,
+}: {
+  conversation: TXMTPConversation;
+}): Promise<TMessage[]> {
+  //Creates a new conversation with the address
+  const messages = await conversation.messages();
+  return formatMessagesXMTP({ messages });
+};
+
+export const sendMessageXMTP = async function ({
+  conversation,
+  message,
+}: {
+  conversation: any;
+  message: TXMTPConversation;
+}): Promise<void> {
+  await conversation.send(message);
+};
+
+export const getConversationsListXMTP = async function ({
+  xmtp_client,
+}: {
+  xmtp_client: TXMTPClient;
+}): Promise<TXMTPConversation[]> {
+  const conversations = await xmtp_client.conversations.list();
+  return conversations;
+};
+
+export const formatMessagesXMTP = async function ({
+  messages,
+}: {
+  messages: TXMTPMessage[];
+}): Promise<TMessage[]> {
+  const formattedMessages: TMessage[] = [];
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    const formattedMessage: TMessage = {
+      id: message.id,
+      senderAddress: message.senderAddress,
+      recipientAddress: message.recipientAddress || "",
+      sent: message.sent,
+      content: message.content.toString(),
+      platform: "xmtp",
+    };
+    formattedMessages.push(formattedMessage);
+  }
+  return formattedMessages;
+};
+
+export const streamMessagesXMTP = async ({
+  conversation,
+  olderMessages,
+}: {
+  conversation: TXMTPConversation;
+  olderMessages: TMessage[]; //| TXMTPMessage[];
+}) => {
+  const newStream = await conversation.streamMessages();
+  for await (const newMsg of newStream) {
+    const isNew = olderMessages.find((m) => m.id === newMsg.id);
+    if (isNew) {
+      // setMessages((prevMessages) => {
+      //   const msgsnew = [...prevMessages, newMsg];
+      //   return msgsnew;
+      // });
+      // updateMessages(oldMessages, newMsg);
+    }
+  }
+};
