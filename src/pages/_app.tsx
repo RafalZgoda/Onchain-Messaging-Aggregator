683352cbc6dd40b256a9d9d4e88afbe881c587cd
KRD <kaimorciniec@gmail.com>
2023-07-21 19:56:19 +0200

    Migrate to typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import "../../styles/globals.css";
+import Head from "next/head";
+import Layout from "components/Layout";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<title>Tailwind Some Works</title>
+				<meta name="description" content="Tailwind Some Works" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
+}
+
+export default MyApp;
