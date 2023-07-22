fcfb219234c63a8a8ab296363d308269749d0a92
KRD <kaimorciniec@gmail.com>
2023-07-22 15:49:01 +0200

    Use Next image for logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Logo = () => {
 	return (
 		<Link href="/">
 			<h1 className="font-bold text-white text-5xl text-center hover:cursor-pointer">
-				<img src="img/logo.png" className="object-contain w-52 mt-[-25px]"/>
+				<Image
+					alt="S3ND"
+					src="img/logo.png"
+					className="object-contain w-52 mt-[-25px]"
+				/>
 			</h1>
 		</Link>
 	);
