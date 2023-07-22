b2e289fb5a326fa7290f436f761ee6a804e0f5b5
Madderstone <anthony.issa@hotmail.fr>
2023-07-22 16:53:06 +0200

    wip

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -14,7 +14,7 @@ import {
 } from "libs";
 import Router from "next/router";
 import { JsonRpcSigner } from "@ethersproject/providers";
-import { useAccount } from "wagmi";
+import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
 
 export default function Chat({
   xmtp,
@@ -34,9 +34,11 @@ export default function Chat({
   const [platformsFilterVisibility, setPlatformsFilterVisibility] =
     useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [ensAvartUrl, setEnsAvartUrl] = useState("");
+
 
   useEffect(() => {
-    if (!xmtp || !signer)return
+    if (!xmtp || !signer) return;
     getConversations();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [xmtp, signer]);
@@ -129,13 +131,13 @@ export default function Chat({
 
   return (
     <>
-      <div className="h-screen">
+      <div className="h-[calc(100vh-60px)] ">
         <main className="flex h-full">
-          <div className="w-full h-full rounded-md bg-telegram-gray-300 shadow-lg shadow-gray-800 ">
+          <div className="w-full h-full rounded-md bg-[#1F1F23] shadow-lg shadow-gray-800 ">
             <header></header>
             <div className="grid grid-cols-3 h-full">
               <div className="col-span-1 overflow-hidden">
-                <div className="flex items-center bg-telegram-gray-300 pl-2">
+                <div className="flex items-center bg-[#1F1F23] pl-2">
                   <div className="text-telegram-gray-100">
                     <SvgGenerator
                       path="M4 6h16M4 12h16M4 18h16"
@@ -146,7 +148,7 @@ export default function Chat({
                     <input
                       type="text"
                       placeholder="Search"
-                      className="w-full text-white text-xs p-2.5 rounded-sm outline-none bg-telegram-gray-200"
+                      className="w-full text-white text-xs p-2.5 rounded-sm outline-none bg-[#1F1F23]"
                     />
                   </div>
                 </div>
@@ -174,29 +176,26 @@ export default function Chat({
                   )}
                 </div>
               </div>
-              <div className="flex flex-col justify-between col-span-2">
-                <div className="flex items-center justify-between p-2 bg-telegram-gray-300">
+              <div className="flex flex-col justify-between col-span-2 m-5">
+                <div className="flex items-center justify-between px-5 pt-3 bg-[#26282D] rounded-t-[30px] border-b">
                   <div className="flex items-center">
-                    <div className="ml-2">
-                      <h2 className="font-medium text-white text-sm">
-                        {activeConversation?.addressTo}
-                      </h2>
-                      <p className=" text-telegram-gray-100 text-xs">
-                        last message at{" "}
-                        {activeConversation?.lastMessageDate.toDateString()}
-                      </p>
+                    <div className="ml-2 flex items-center">
+                      <img
+                        className="rounded-full w-10 h-10 mr-3"
+                        src={ensAvartUrl ?? "/img/eth.png"}
+                      ></img>
+                      <div>
+                        <h2 className="font-medium text-white text-sm m-0">
+                          {activeConversation?.addressTo}
+                        </h2>
+                        <p className=" text-telegram-gray-100 text-xs m-0">
+                          last message at{" "}
+                          {activeConversation?.lastMessageDate.toDateString()}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex justify-around w-40 text-telegram-gray-100">
-                    <SvgGenerator // logo search
-                      path="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                      className="w-5 h-5"
-                    />
-                    <SvgGenerator // logo phone
-                      path="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                      className="w-5 h-5"
-                    />
-
+                  <div className="w-40 text-telegram-gray-100 flex justify-end">
                     <button
                       onClick={() => {
                         setPlatformsFilterVisibility(
@@ -246,22 +245,18 @@ export default function Chat({
                   </div>
                 )}
 
-                <div className="relative pb-3 w-full h-full overflow-y-scroll flex justify-end flex-col bg-telegram-gray-400">
+                <div className="relative pb-3 w-full h-full overflow-y-scroll flex justify-end flex-col bg-[#26282D]">
                   {filteredMessages.map((message, index) => (
                     <div key={index}>
                       <Messages message={message} />
                     </div>
                   ))}
                 </div>
-                <div className="flex p-1 bg-telegram-gray-300">
-                  <SvgGenerator
-                    path="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                    className="mx-2 w-7 top-2 left-2 text-telegram-gray-100"
-                  />
+                <div className="flex p-1 bg-[#26282D] rounded-b-[30px] px-5 pb-3">
                   <input
                     type="text"
                     placeholder="Write a message..."
-                    className="w-full text-white text-xs p-2.5 rounded-md outline-none bg-telegram-gray-300"
+                    className="w-full text-white text-xs p-2.5 rounded-md outline-none bg-[#3F4249] border-none mx-5"
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                     onKeyDown={(e) => {
@@ -270,15 +265,25 @@ export default function Chat({
                       }
                     }}
                   />
-                  <button onClick={() => handleSendMessage()}>Send</button>
-                  <SvgGenerator
-                    path="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    className="mr-1 w-9 top-2 left-2 text-telegram-gray-100"
-                  />
-                  <SvgGenerator
-                    path="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
-                    className="mx-2 w-8 top-2 left-2 text-telegram-gray-100"
-                  />
+                  <button
+                    onClick={() => handleSendMessage()}
+                    className="bg-[#3C8AFF] border-none rounded-[100px] h-8 cursor-pointer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6 mt-[2px]"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
             </div>
