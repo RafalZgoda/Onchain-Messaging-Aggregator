3bb905e62ad0b12436967c652fd2e574a9e15107
KRD <kaimorciniec@gmail.com>
2023-07-22 14:02:08 +0200

    Make logo link to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const Logo = () => {
-  return (
-    <h1 className="font-bold text-white text-5xl text-center">
-      S<span className="text-[#3C8AFF]">3</span>ND
-    </h1>
-  );
+	return (
+		<Link href="/">
+			<h1 className="font-bold text-white text-5xl text-center hover:cursor-pointer">
+				S<span className="text-[#3C8AFF]">3</span>ND
+			</h1>
+		</Link>
+	);
 };
