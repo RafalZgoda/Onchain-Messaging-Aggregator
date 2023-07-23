0fe3e1aa993c73cee5bf033547c02205031ca49f
KRD <kaimorciniec@gmail.com>
2023-07-23 05:28:31 +0200

    Add sismo button to settings

--- src/components/config-sismo.ts
+++ src/components/config-sismo.ts
@@ -1,27 +1,25 @@
 import { SismoConnectConfig } from "@sismo-core/sismo-connect-react";
 
 export const config: SismoConnectConfig = {
-  appId: "0x32403ced4b65f2079eda77c84e7d2be6",
-  vault: {
-    // For development purposes insert the Data Sources that you want to impersonate
-    // Never use this in production
-    impersonate: [
-      // EVM Data Sources
-      "dhadrien.sismo.eth",
-      "leo21.sismo.eth",
-      "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
-      "0x1b9424ed517f7700e7368e34a9743295a225d889",
-      "0x82fbed074f62386ed43bb816f748e8817bf46ff7",
-      "0xc281bd4db5bf94f02a8525dca954db3895685700",
-      "vitalik.eth",
-      // Github Data Source
-      "github:dhadrien",
-      // Twitter Data Source
-      "twitter:dhadrien_",
-      // Telegram Data Source
-      "telegram:dhadrien",
-    ],
-  },
-  displayRawResponse: true, // this enables you to get access directly to the
-  // Sismo Connect Response in the vault instead of redirecting back to the app
+	appId: "0x32403ced4b65f2079eda77c84e7d2be6",
+	vault: {
+		// For development purposes insert the Data Sources that you want to impersonate
+		// Never use this in production
+		impersonate: [
+			// EVM Data Sources
+			"dhadrien.sismo.eth",
+			"leo21.sismo.eth",
+			"0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
+			"0xc281bd4db5bf94f02a8525dca954db3895685700",
+			"vitalik.eth",
+			// Github Data Source
+			"github:dhadrien",
+			// Twitter Data Source
+			"twitter:dhadrien_",
+			// Telegram Data Source
+			"telegram:dhadrien",
+		],
+	},
+	displayRawResponse: true, // this enables you to get access directly to the
+	// Sismo Connect Response in the vault instead of redirecting back to the app
 };
